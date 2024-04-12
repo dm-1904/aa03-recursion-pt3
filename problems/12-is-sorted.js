470deeca2492289519cfa43f0a22bc51bd186d13
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-04-11 23:33:18 -0400

    problem 12 solved

--- problems/12-is-sorted.js
+++ problems/12-is-sorted.js
@@ -12,9 +12,21 @@ isSorted([5, 4, 3, 2, 1]); // false
 
 
 function isSorted(arr) {
-  // Your code here 
+  if (arr.length === 1) return true
+  
+  // if condition: arr + 1 === currNum + 1?
+  // arr[arr + 1] === arr[0]? if true arr.slice(1)
+  if (arr[1] > arr[0]) {
+    let restOfArr = arr.slice(1)
+    return isSorted(restOfArr)
+  }
+  
+  return false
 }
-
+console.log(isSorted([1, 2, 3, 4, 5])); // true
+console.log(isSorted([1, 2, 4, 3, 5])); // false
+console.log(isSorted([2, 4, 6, 7, 8])); // true
+console.log(isSorted([5, 4, 3, 2, 1])); // false
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
